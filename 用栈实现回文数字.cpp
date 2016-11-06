d46b429f036398c0026c1bd6a38a44051c9257c9
鱼宁 <1578517699@qq.com>
2016-11-06 23:05:35 +0800

    添加注释
    Signed-off-by: 鱼宁 <1578517699@qq.com>

--- 用栈实现回文数字.cpp
+++ 用栈实现回文数字.cpp
@@ -9,28 +9,28 @@ typedef struct
 	int top;
 }SeqStack;
 
-SeqStack *init_SeqStack();
-int Empty_SeqStack(SeqStack * s);
-int Push_SeqStack(SeqStack * s,char x);
-int Pop_SeqStack(SeqStack * s,char * x);
+SeqStack *init_SeqStack();//建立栈 
+int Empty_SeqStack(SeqStack * s);//判断栈是否为空 
+int Push_SeqStack(SeqStack * s,char x);//进行x的入栈 
+int Pop_SeqStack(SeqStack * s,char * x);//出栈，并将出栈的元素存入x中 
 
 SeqStack *init_SeqStack()
 {
 	SeqStack * s;
-	s=(SeqStack *)malloc(sizeof(SeqStack));
-	s->top=-1;
+	s=(SeqStack *)malloc(sizeof(SeqStack));//为栈分配空间 
+	s->top=-1;//初始top值赋为-1 
 	return s;	
 }
 int Empty_SeqStack(SeqStack * s)
 {
 	if(s->top==-1)
-		return 1;
+		return 1;//若栈空，返回1 
 	else
 		return 0; 
 }
 int Push_SeqStack(SeqStack * s,char x)
 {
-	if(s->top==maxsize-1)
+	if(s->top==maxsize-1)//若栈满，返回0 
 	return 0;
 	else{
 		s->top++;
@@ -43,7 +43,7 @@ int Pop_SeqStack(SeqStack * s,char * x)
 	if(Empty_SeqStack(s)==1)
 		return 0;
 	else{
-		*x=s->data[s->top];
+		*x=s->data[s->top];//将栈顶元素赋给x 
 		s->top--;
 		return 1;
 	}
@@ -51,28 +51,28 @@ int Pop_SeqStack(SeqStack * s,char * x)
 
 int main(void)
 {
-	char a[100]={0};
+	char a[100]={0};//初始化数组 
 	char b[100]={0}; 
 	int i,j;
-	gets(a);
+	gets(a);//输入一串字符，以@结束 
 	SeqStack * s;
 	s=init_SeqStack();
-	for(i=0;a[i]!='@';i++)
+	for(i=0;a[i]!='@';i++)//将这一串字符入栈，‘@’没有入栈 
 	{
 		j=Push_SeqStack(s,a[i]);
 		if(j==0)
 			break;
 	}
-	for(i=0;;i++)
+	for(i=0;;i++)//再出栈到另一个数组中 
 	{
 		j=Pop_SeqStack(s,&b[i]);
 		if(j==0)
 			break;
 	}
-	b[i]='@';
-	if(strcmp(a,b)==0)
-		printf("回文数字！");
+	b[i]='@';//再将‘@’赋给b[i] 
+	if(strcmp(a,b)==0)//判断两个数组是否相同 
+		printf("回文数字！\n");
 	else
-		printf("不是回文数字！");
+		printf("不是回文数字！\n");
 	return 0;	
 } 
