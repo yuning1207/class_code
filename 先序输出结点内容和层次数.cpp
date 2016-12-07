bbc1124c4c76f8444d3023a508835dfdd6404f86
鱼宁 <1578517699@qq.com>
2016-12-07 14:44:37 +0800

    .
    Signed-off-by: 鱼宁 <1578517699@qq.com>

---
+++ 先序输出结点内容和层次数.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node 
+{
+	char data;
+	struct node * lchild;
+	struct node * rchild;
+}bitnode;
+
+void creatbitree(bitnode ** root);//创建二叉树 
+void previsittree(bitnode * root,int level);//先序输出 
+
+void previsittree(bitnode * root,int level)
+{
+	if(root)
+	{
+		printf("(%c,%d)",root->data,level);
+		previsittree(root->lchild,level+1);
+		previsittree(root->rchild,level+1);	
+	}
+}
+void creatbitree(bitnode ** root)
+{
+	char ch;
+	ch=getchar();
+	if(ch=='#')
+		* root=NULL;
+	else
+	{
+		*root=(bitnode *)malloc(sizeof(bitnode));
+		if(*root==NULL)
+			exit(-1);
+		(*root)->data=ch;
+		creatbitree(&((*root)->lchild));
+		creatbitree(&((*root)->rchild));
+	}
+} 
+
+int main(void)
+{
+	bitnode * root;
+	int level=1;
+	root=(bitnode *)malloc(sizeof(bitnode));
+	creatbitree(&root);
+	previsittree(root,level);
+	return 0;
+}
