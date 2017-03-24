26ee46de094d2788874df18aafd63be094eaf907
鱼宁 <1578517699@qq.com>
2017-03-24 11:46:34 +0800

    改正
    Signed-off-by: 鱼宁 <1578517699@qq.com>

--- c++引用练习.cpp
+++ c++引用练习.cpp
@@ -2,7 +2,7 @@
 void f(int *a,int n,int &max,int &min)
 {
 	max=min=a[9];
-	for(int i=1;i<n;i++)
+	for(int i=0;i<n;i++)
 	{
 		if(max<a[i])
 			max=a[i];
@@ -12,8 +12,11 @@ void f(int *a,int n,int &max,int &min)
 } 
 int main(void)
 {
-	int a[10]={2,5,3,9,0,8,1,7,6,4};
+	int a[10],i;
 	int max,min;
+	for(i=0;i<10;i++){
+     cin>>a[i];
+    }
 	f(a,10,max,min);
 	cout<<"Max: "<<max<<endl;
 	cout<<"Min: "<<min<<endl;
